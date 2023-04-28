0f6b5096058d1bb274c603a0dc4534f24d1ae725
Harshika Govind <harshika0102@gmail.com>
2023-04-28 19:18:13 +0530

    add reservation counter

--- src/Modules/reservationPopUp.js
+++ src/Modules/reservationPopUp.js
@@ -1,3 +1,5 @@
+import reservationCounter from "./reservationCounter";
+
 const getReservation = async (itemId) => {
   try {
     const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/zRod3rPxBRjxEDaYzujw/reservations?item_id=${itemId}`);
@@ -54,6 +56,8 @@ const reservationPopUp = () => {
     popupTitle.innerHTML = data.title;
 
     const commentsTitle = document.createElement('h3');
+    commentsTitle.id= 'reservation-counter'
+   
     const reservations = getReservation(data.key).then((res) => {
       if (res.error) {
         commentsTitle.textContent = 'Reservations (0)';
@@ -75,6 +79,7 @@ const reservationPopUp = () => {
     commentsTitle.textContent = `Reservations (${reservations === undefined ? '0' : reservations?.length})`;
 
     const commentList = document.createElement('ul');
+    commentList.id ='comment-list';
     commentList.classList.add('comment-list');
 
     if (reservations.length > 0) {
