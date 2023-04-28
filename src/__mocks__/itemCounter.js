e0fc828b37d579fc78197adbd6e0ca6263c8ebf2
yusufobr <yousarsif80@gmail.com>
2023-04-28 11:28:34 +0100

    add counter test

---
+++ src/__mocks__/itemCounter.js
@@ -0,0 +1,12 @@
+const itemsContainer = document.getElementById('cards');
+
+export const itemsCounter = (item) => {
+  
+  const childCount = item.childElementCount;
+  const charts = document.createElement('li');
+  
+  charts.innerHTML = `Top Charts (${childCount})`;
+  
+};
+
+exports.itemsCounter = itemsCounter;
---
+++ src/test/itemCounter.test.js
@@ -0,0 +1,47 @@
+import itemCounter from '../Modules/itemCounter.js'
+
+
+describe('itemCounters', () => {
+  test('Counts an empty array', () => {
+    const Items = [];
+    const itemCounters = document.createElement('li');
+    itemCounter(Items, itemCounters);
+    expect(itemCounter.innerHTML).toBe('');
+  });
+  test('Counts the correct items', () => {
+    const itemCounter = document.createElement('li');
+    const JsonItems = [
+      {
+        item_id: 29291,
+        likes: 81,
+      },
+      {
+        item_id: 29291,
+        likes: 81,
+      },
+      {
+        item_id: 29291,
+        likes: 81,
+      },
+    ];
+    itemCounters(JsonItems, itemCounter);
+
+    expect(itemCounter.innerHTML).toBe('Food(3)');
+  });
+  test('Counts the correct items', () => {
+    const itemCounter = document.createElement('li');
+    const JsonItems = [
+      {
+        item_id: 29291,
+        likes: 81,
+      },
+      {
+        item_id: 29291,
+        likes: 81,
+      },
+    ];
+    itemCounters(JsonItems, itemCounter);
+
+    expect(itemCounter.innerHTML).toBe('Food(2)');
+  });
+});
